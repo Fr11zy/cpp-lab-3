b30650c8a6633436893ad0b9c552beceb098c341
freezy <freezy@mail.com>
2023-10-09 12:17:06 +0300

    Using recursion function

--- lab3.cpp
+++ lab3.cpp
@@ -2,14 +2,17 @@
 #include <cstdlib>
 using namespace std;
 
-int find_difference(int i,int n,int* all_plates, int summa1, int summa2)
+int n;
+int all_plates[32];
+
+int find_difference(int i, int summa1, int summa2)
 {
-    if ((i==n) || (all_plates[i]==0))
+    if (i==n)
         return abs(summa1-summa2);
     else
     {
-        int masha_plate=find_difference(++i,n,all_plates,summa1+all_plates[i],summa2);
-        int petya_plate=find_difference(++i,n,all_plates,summa1,summa2+all_plates[i]);
+        int masha_plate=find_difference(i+1,summa1+all_plates[i],summa2);
+        int petya_plate=find_difference(i+1,summa1,summa2+all_plates[i]);
         if (masha_plate<petya_plate)
             return masha_plate;
         else
@@ -19,11 +22,9 @@ int find_difference(int i,int n,int* all_plates, int summa1, int summa2)
 
 int main()
 {   
-    int n;
     cin >> n;
-    int all_plates[32];
     for (int i=0;i<n;i++)
         cin >> all_plates[i];
-    int difference=find_difference(0,n,all_plates,0,0);
+    int difference=find_difference(0,0,0);
     cout << difference;
 }
